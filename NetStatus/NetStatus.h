19988aeb3e00aa2261731b26fc733115f469bd9e
Nurul Karim Rafi <nurul.karim@bs-23.net>
2019-08-08 11:28:27 +0600

    init

---
+++ NetStatus/NetStatus.h
@@ -0,0 +1,19 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by Nurul Karim Rafi on 8/1/19.
+//  Copyright © 2019 Nurul Karim Rafi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
